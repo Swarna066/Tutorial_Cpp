339bba56e8e3c0ed88ab4ad07dc214d7da43f90f
Swarna066 <230779529+Swarna066@users.noreply.github.com>
2026-05-27 00:59:46 +0530

    Unordered_map hash

--- DSA/hashing.cpp
+++ DSA/hashing.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include <map>
+#include <unordered_map>
 using namespace std;
 
 int main(){
     int  n;
     cin >> n;
     int arr[n];
-    map<int, int> mpp;
+    unordered_map<int, int> mpp;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
